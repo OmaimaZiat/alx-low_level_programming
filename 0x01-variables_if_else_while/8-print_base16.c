306f7091a3ed3305e6bbeff23b59828374231718
OmaimaZiat <ziat.omaima9301@gmail.com>
2023-03-17 16:49:28 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - main function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char y;
+
+for (i = 0; i < 10 ; i++)
+	putchar(i + '0');
+for (y = 'a' ; y <= 'f' ; y++)
+	putchar(y);
+putchar('\n');
+return (0);
+}
